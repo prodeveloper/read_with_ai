dbeab10bafa025e3d300f1dbee5ebddde8742ec2
Chencha Jacob <jmokua@gmail.com>
2024-07-05 15:57:55 +0100

    Allow key input from user

--- stream.py
+++ stream.py
@@ -14,18 +14,18 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-def initialize_services(file_name):
+def initialize_services(file_name,gemini_key):
     pdf_path = FilePath(path=file_name)
-    gemini_setup = GeminiSetup(api_key=os.getenv("GEMINI_API_KEY"), model="gemini-1.5-flash")
+    gemini_setup = GeminiSetup(api_key=gemini_key, model="gemini-1.5-flash")
     return PDFConverse(pdf_path=pdf_path, gemini_setup=gemini_setup)
 
-def handle_file_uploaded(uploaded_file,st_file,prompt):
+def handle_file_uploaded(uploaded_file,st_file,prompt,gemini_key):
     # Save the uploaded file to a temporary location
     file_name = PresentationService.generate_unique_file_name(uploaded_file)
     with open(file_name, "wb") as f:
         f.write(st_file.getbuffer())
     # Initialize PDFConverse
-    pdfconverse = initialize_services(file_name)
+    pdfconverse = initialize_services(file_name,gemini_key)
     page_to_summarize = st.number_input("Enter the page to summarize:", value=1)
     first_page = last_page = page_to_summarize - 1
     summary = PresentationService.get_summary(pdfconverse, first_page, last_page, uploaded_file,prompt)
@@ -36,10 +36,11 @@ st.write("This app reads with you giving you summary of current page")
 # Allow user to upload a PDF file
 st_file = st.file_uploader("Choose a PDF file", type="pdf")
 prompt = st.text_input("Enter a prompt:", value="Explain this to me concisely maximum 5 bullet points as simply as possible")
-
+existing =os.getenv("GEMINI_API_KEY") if os.getenv("GEMINI_API_KEY") else "REPLACE WITH VALID GEMINI KEY"
+gemini_key = st.text_input("Enter your Gemini key:", value=existing, type="password")
 if st_file is not None:
     data = st_file.getvalue()
     uploaded_file = UploadedFile(name=st_file.name, type=st_file.type, size=st_file.size, data=data)
-    handle_file_uploaded(uploaded_file,st_file,prompt)
+    handle_file_uploaded(uploaded_file,st_file,prompt,gemini_key)
 else:
     st.write("Please upload a PDF file to proceed.")
