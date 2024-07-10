20d0207b2499118df9cf6b683b165bdb853b0fa2
Chencha Jacob <jmokua@gmail.com>
2024-07-10 16:28:47 +0100

    added messaging for failed errors

--- controllers/webcheck.py
+++ controllers/webcheck.py
@@ -5,12 +5,14 @@ def firebase_works():
         cloud_storage_test.test_firebase_integration()
         return True
     except Exception as e:
+        st.write("Firebase failed with message",e)
         return False
 def pdf_converse_works():
     try:
         cloud_storage_test.test_pdf_converse_integration()
         return True
     except Exception as e:
+        
         return False
 def pdf_converse_works_by_bytes():
     try:
